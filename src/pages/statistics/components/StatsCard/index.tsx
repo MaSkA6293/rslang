43702a28022c593d087dac27eb256a1c1911429c
Nika Perfilova <ees2009.2014@gmail.com>
2022-08-28 19:34:14 +0700

    feat: show words statistics for today

---
+++ src/pages/statistics/components/StatsCard/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+interface StatsCardProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+function StatsCard({ title, children }: StatsCardProps) {
+  return (
+    <Card className="h-100" style={{ width: '18rem' }}>
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="fw-bold fs-3">{title}</Card.Title>
+        <div className="fs-1 fw-bold my-auto">{children}</div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default StatsCard;
--- src/pages/statistics/components/statistics/index.tsx
+++ src/pages/statistics/components/statistics/index.tsx
@@ -1,6 +1,8 @@
 import { Row, Col, Container } from 'react-bootstrap';
+import { IWordsStatistics } from '../../../../API/types';
 import { useGetUserStatisticQuery } from '../../../../API/userApi';
 import GameStatsCard from '../GameStatsCard';
+import StatsCard from '../StatsCard';
 
 interface StatisticsProps {
   userId: string;
@@ -16,35 +18,79 @@ const gamesData = [
   { id: Games.sprint, title: 'Спринт' },
 ];
 
+const today = new Date().toISOString().slice(0, 10);
+
+const defaultWordsStats: IWordsStatistics = {
+  newWords: [],
+  learnedWords: 0,
+  correctAnswers: 0,
+  totalAnswers: 0,
+};
+
 function Statistics({ userId }: StatisticsProps) {
   const { data, isLoading } = useGetUserStatisticQuery({ userId });
 
+  const wordsStats =
+    data?.optional.wordsStatsByDate[today] || defaultWordsStats;
+
+  const correctAnwersPcnt =
+    Math.round((wordsStats.correctAnswers / wordsStats.totalAnswers) * 100) ||
+    0;
+
   return (
     <Container>
-      <h2 className="fw-bold">Статистика за сегодня</h2>
+      <h2 className="fw-bold my-5">Статистика за сегодня</h2>
 
       {isLoading || !data ? (
         'Loading...'
       ) : (
-        <Row
-          xs={1}
-          md={2}
-          className="mx-auto g-4"
-          style={{ maxWidth: '40rem' }}
-        >
-          {gamesData.map((game) => {
-            const gameStats = data.optional.games[game.id];
-            return (
-              <Col key={`${game.id}-col`}>
-                <GameStatsCard
-                  key={game.id}
-                  gameTitle={game.title}
-                  statistics={gameStats}
-                />
-              </Col>
-            );
-          })}
-        </Row>
+        <>
+          <Row
+            xs={1}
+            md={2}
+            lg={3}
+            className="mx-auto g-4 mb-5 justify-content-center"
+            style={{ maxWidth: '62rem' }}
+          >
+            <Col>
+              <StatsCard title="Новых слов">
+                {wordsStats.newWords.length}
+              </StatsCard>
+            </Col>
+
+            <Col>
+              <StatsCard title="Изучено слов">
+                {wordsStats.learnedWords}
+              </StatsCard>
+            </Col>
+
+            <Col>
+              <StatsCard title="Правильных ответов">
+                {`${correctAnwersPcnt}%`}
+              </StatsCard>
+            </Col>
+          </Row>
+
+          <Row
+            xs={1}
+            md={2}
+            className="mx-auto g-4"
+            style={{ maxWidth: '40rem' }}
+          >
+            {gamesData.map((game) => {
+              const gameStats = data.optional.games[game.id];
+              return (
+                <Col key={`${game.id}-col`}>
+                  <GameStatsCard
+                    key={game.id}
+                    gameTitle={game.title}
+                    statistics={gameStats}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+        </>
       )}
     </Container>
   );
